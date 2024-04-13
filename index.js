d9670faf75231cc32273c18f88fa3b164f62bb9e
omargalil0x1 <omargalil42@gmail.com>
2024-04-13 18:00:14 +0200

    added cors and json middlewares

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+
 
 // import controllers.
 const findJob = require('./controllers/findJob.js')
 
 const app = express()
+
+app.use(express.json())
+app.use(cors())
 
 app.get('/api/find-job', findJob.index) // finding the first page jobs.
 app.get('/api/find-job/:page_number', findJob.index) // search in specific page.
